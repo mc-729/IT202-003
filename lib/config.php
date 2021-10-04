6a7196ae391116843deab88593c9a6e979255738
mc729 <mc729@njit.edu>
2021-10-04 10:31:32 -0400

    Initial Commit

--- lib/config.php
+++ lib/config.php
@@ -7,10 +7,10 @@ if($ini && isset($ini["DB_URL"])){
 }
 else{
     //load from heroku env variables
-    $db_url      = parse_url(getenv("DB_URL"));
+    $db_url = parse_url(getenv("DB_URL"));
 }
-$dbhost   = $db_url["host"];
+$dbhost = $db_url["host"];
 $dbuser = $db_url["user"];
 $dbpass = $db_url["pass"];
-$dbdatabase       = substr($db_url["path"],1);
+$dbdatabase = substr($db_url["path"],1);
 ?>
