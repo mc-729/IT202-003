b5c70fc47dd9cad7b93f3855f71aedd7e457a9e8
mc729 <mc729@njit.edu>
2021-10-25 10:43:26 -0400

    Adding and committing files that were modified and added from Fleshing Out Milestone 1 presentation

--- public_html/Project/logout.php
+++ public_html/Project/logout.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-session_unset();
-session_destroy();
 require(__DIR__ . "/../../lib/functions.php");
-flash("Successfully logged out", "Success!");
+reset_session();
+
+flash("Successfully logged out", "success");
 header("Location: login.php");
