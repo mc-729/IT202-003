ea520a9c3ad25ccd5321f1da17bfb09cae874dcf
mc729 <mc729@njit.edu>
2021-11-30 22:57:35 -0500

    Finished Feat-AddProducts

--- public_html/Project/sql/005_insert_roles_admin.sql
+++ public_html/Project/sql/005_insert_roles_admin.sql
@@ -1,1 +1,2 @@
 INSERT INTO Roles(id, name, is_active) VALUES(1, 'Admin', 1);
+INSERT INTO Roles(id, name, is_active) VALUES(2, 'Shop Owner', 1);
--- public_html/Project/sql/006_create_table_Products.sql
+++ public_html/Project/sql/006_create_table_Products.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS `Products` (
-    `id` int AUTO_INCREMENT PRIMARY  KEY,
-    `name` varchar(20) UNIQUE,
+    `id` INT AUTO_INCREMENT PRIMARY  KEY,
+    `name` VARCHAR(30) UNIQUE,
     `description` TEXT,
-    `category` 
+    `category` VARCHAR(20),
     `stock` INT DEFAULT 0,
     `created`  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `modified` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `unit_price`
-    `visibility`
+    `unit_price` DECIMAL(6,2),
+    `visibility` TINYINT(1)
 )
