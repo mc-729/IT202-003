86a89d1f08a92c0cccbd68a57c536a7400d0db36
mc729 <mc729@njit.edu>
2021-12-13 22:33:39 -0500

    Feat-Cart created and some modifications

--- public_html/Project/sql/007_create_table_Cart.sql
+++ public_html/Project/sql/007_create_table_Cart.sql
@@ -1,10 +1,9 @@
 CREATE TABLE IF NOT EXISTS `Cart` (
     `id` INT AUTO_INCREMENT PRIMARY  KEY,
-    `product_id` ,
-    `user_id` ,
-    `desired_quantity` ,
-    `unit_cost` ,
+    `product_id` int,
+    `user_id` int,
+    `desired_quantity` int,
+    `unit_cost` DECIMAL(6,2),
     `created`  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `modified` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `unit_price` , 
 )
